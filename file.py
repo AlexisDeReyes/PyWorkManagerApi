7979fafdeb4f8fcd945660d1310d8e1bc3492724
AlexisDeReyes <arreyes20285@gmail.com>
2020-01-03 12:46:30 -0500

    just adding docstrings to my code

--- file.py
+++ file.py
@@ -1,8 +1,25 @@
+#!/usr/bin/env python3
+
+"""Retrieves and prints words from a url
+
+Usage:
+
+    python file.py <URL>
+"""
+
 from urllib.request import urlopen
 import sys
 
 
 def fetch_words(url):
+    """Fetches a list of words from a url
+
+    Arguments:
+        url {string} -- url you wish to retrieve words from
+
+    Returns:
+        list -- list of words decoded into string format
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -13,14 +30,28 @@ def fetch_words(url):
 
 
 def print_words(story_words):
+    """Prints a list of words
+
+    Arguments:
+        story_words {list} -- list of words you wish to print
+    """
     for words in story_words:
         print(words)
 
 
 def main(url):
+    """Main method, fetches words and prints them from a specific url
+
+    Arguments:
+        url {string} -- url you wish to print words from
+    """
     words = fetch_words(url)
     print_words(words)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    # argv[0] is the filename so we'll use the first one
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        main('http://sixty-north.com/c/t.txt')
