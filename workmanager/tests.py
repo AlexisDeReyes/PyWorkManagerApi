c04b59ab8884f5ccbe87c0f3ac3dc241c04c9e8d
AlexisDeReyes <arreyes20285@gmail.com>
2020-01-27 11:35:15 -0500

    trying out some testing, I need to learn more here and return

--- workmanager/tests.py
+++ workmanager/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+def test_sum():
+    assert sum([1, 2, 3]) == 6, "Should be 6"
+
+
+if __name__ == "__main__":
+    test_sum()
+    print("Everything passed")
